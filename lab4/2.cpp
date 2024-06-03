2f1846e7d3ecb2f7e757fd5be639ad23710e82c0
sarykush <pituhkukarekukudahtahtah@gmail.com>
2024-06-03 23:09:35 +0300

    2.cpp

--- lab4/2.cpp
+++ lab4/2.cpp
@@ -1,4 +1,3 @@
-//Метод Якоби с произвольной размерностью матрицы
 #include <iostream>
 using namespace std;
 
@@ -11,7 +10,60 @@ void print_vect(double *v){
     }
     cout<<"\n";
 }
+
+void print_matr(double **matr){
+    for (int i=0;i<N;i++){
+        for(int k=0; k<N;k++){
+            cout<<matr[i][k]<<" ";
+        }
+        cout<<"\n";
+
+    }
+    cout<<"\n";
+}
  
+//функция вычеркивания строки и столбца
+void kill(double **matr, int n, double **temp_matr, int indRow, int indCol)   
+{
+    int ki = 0; 
+    for (int i = 0; i < n; i++){
+        if(i != indRow){
+            for (int j = 0, kj = 0; j < n; j++){
+                if (j != indCol){
+                    temp_matr[ki][kj] = matr[i][j];
+                    kj++;
+                }
+            }
+            ki++;       
+        }
+    }
+}
+ 
+
+//функция вычисления определителя матрицы
+double det(double **matr, int n)  
+{
+    double temp=0; 
+    int k = 1; 
+    if (n == 1)
+        temp = matr[0][0];
+    else if (n == 2)
+        temp = matr[0][0] * matr[1][1] - matr[1][0] * matr[0][1];
+    else{
+        for(int i = 0; i < n; i++){
+            int m = n - 1; 
+            double **temp_matr = new double * [m];
+            for(int j = 0; j < m; j++)
+                temp_matr[j] = new double [m];
+            kill(matr, n, temp_matr, 0, i);
+            temp = temp + k * matr[0][i] * det(temp_matr, m);
+            k = -k;
+        }
+    }
+    k=0;
+    return temp;
+}
+
 double* mul_vect(double**matr, double*vect){
     for (int p = 0; p < N; p++)
     {
@@ -94,5 +146,45 @@ int main()
     
     cout<<"x1:      x2:       x3:       "<<endl;
     print_vect(xx);
+    
+    double** submatr_x1 = new double*[d];
+    for (int i = 0; i < d; i++) {
+        submatr_x1[i] = new double[d];
+    }
+    for (int i=0; i<d; i++){
+        for (int j=0; j<d; j++){
+            if (j==0) submatr_x1[i][j]=b[i];
+            else submatr_x1[i][j]=matr[i][j];
+        }
+    }
+    
+    double** submatr_x2 = new double*[d];
+    for (int i = 0; i < d; i++) {
+        submatr_x2[i] = new double[d];
+    }
+    for (int i=0; i<d; i++){
+        for (int j=0; j<d; j++){
+            if (j==1) submatr_x2[i][j]=b[i];
+            else submatr_x2[i][j]=matr[i][j];
+        }
+    }
+    
+    double** submatr_x3 = new double*[d];
+    for (int i = 0; i < d; i++) {
+        submatr_x3[i] = new double[d];
+    }
+    for (int i=0; i<d; i++){
+        for (int j=0; j<d; j++){
+            if (j==2) submatr_x3[i][j]=b[i];
+            else submatr_x3[i][j]=matr[i][j];
+        }
+    }
+    
+    double det_main=det(matr,3);
+    double x1=det(submatr_x1,3)/det_main;
+    double x2=det(submatr_x2,3)/det_main;
+    double x3=det(submatr_x3,3)/det_main;
+    cout<<x1<<"   "<<x2<<"   "<<x3<<"   "<<endl;
+    
     return 0;
 }
