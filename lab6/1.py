7b9f5b7b65f10b021d9dccfd8378df5b6c7f93f3
sarykush <pituhkukarekukudahtahtah@gmail.com>
2024-06-03 09:39:52 +0300

    1.py

--- lab6/1.py
+++ lab6/1.py
@@ -9,14 +9,10 @@ x1 = -100
 x_prev = -101
 func = 0
 
-print(f(x1, x2), f(x_prev, x2))
-
 while f(x1, x2) < f(x_prev, x2):
     x_prev = x1
     x1 = x1 + eps
     func = f(x1, x2)
-    print(f"x1={x1}; f={func}")
-    print(f"x_prev={x_prev}")
 
 x1 = x_prev
 x_prev = -101
@@ -25,8 +21,6 @@ while f(x1, x2) < f(x1, x_prev):
     x_prev = x2
     x2 = x2 + eps
     func = f(x1, x2)
-    print(f"x2={x2}; f={func}")
-    print(f"x_prev={x_prev}")
 
 x2 = x_prev
 
